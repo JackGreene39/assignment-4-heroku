d4600b64cfd96211b7b9e361dde0accdae0162d6
JackGreene39 <jackmgreene39@gmail.com>
2020-09-15 20:41:16 -0500

    Initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+    def index
+        @books = Book.all
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+    end
+
+    def edit   
+        @book = Book.find(params[:id])
+    end
+
+    def create
+        @book = Book.new(book_params)
+
+        @book.save
+        redirect_to @book
+    end
+
+    def update
+        @book = Book.find(params[:id])
+ 
+        @book.update(book_params)
+        redirect_to @book
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+       
+        redirect_to books_path
+    end
+
+    private
+        def book_params
+            params.require(:book).permit(:title, :author, :genre, :price, :published_date)
+        end
+end
